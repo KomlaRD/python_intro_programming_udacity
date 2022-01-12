d28b839e2ce15c5db7980c73102b276fd24a5e33
komlaRD <ankueric1@gmail.com>
2022-01-12 20:33:02 +0000

    Create a list

--- organize_photo.py
+++ organize_photo.py
@@ -1,6 +1,12 @@
 # Get a list of the file names
 
 import os
+
+originals = os.listdir()
+
+print (originals)
+
+
 # Extract the place names from the file names
 
 # Make a directory for each place name
